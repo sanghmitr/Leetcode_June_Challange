e43e9d87fba4b0967981fed94fb64e09c358e54b
sanghmitr <sanghmitr.tamrakar@gmail.com>
2020-06-02 12:10:26 +0530

    Day-1 InvertTree

---
+++ day 1-InvertTree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if (root == NULL)  
+            return root;  
+        else
+        { 
+            TreeNode* temp; 
+
+            /* do the subtrees */
+            invertTree(root->left); 
+            invertTree(root->right); 
+
+            /* swap the pointers in this node */
+            temp     = root->left; 
+            root->left = root->right; 
+            root->right = temp; 
+        } 
+        return root;
+    }
+};
